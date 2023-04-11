0676b61a24e7ac59f1e759695bf8c327bf6540a7
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-11 08:29:22 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'definitely bob';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'kinda sorta bob';
+}
